82406e75d45253c0ea5129f9c440db3f24a25df1
Sunil Borate <Sunil Borate@DESKTOP-0VTO70S>
2022-07-18 18:25:16 +0530

    Add project files.

---
+++ NESTED IF/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NESTED_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter email ID...");
+            string email = Console.ReadLine();
+            if (email == "parmeshwarbadad5@gmail.com")
+            {
+               Console.WriteLine("Enter password...");
+               string password = Console.ReadLine();
+                  if (password == "Ittefaq1")
+                  {
+                     Console.WriteLine("Login sucessfulle...");
+                  }
+                  else
+                  {
+                     Console.WriteLine("Incorrect password...");
+                  }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid email ID...");
+
+            }
+        }
+    }
+}
